85dec9e02617d2dd14b4a9490717f8304e692bae
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-10-24 15:53:12 +0100

    Add Media and Book classes

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,24 @@
-// import the Media class:
+const Media = require("./Media");
 
-// create your Book class:
+class Book extends Media {
+    static highestRating(array) {
+        let highestRatedBook = array[0];
+        for (const book of array) {
+            if (book.rating > highestRatedBook.rating) {
+                highestRatedBook = book;
+            }
+        }
+        return highestRatedBook;
+    }
+    constructor(title, genre, year, author, numPages, rating) {
+        super(title, genre, year)
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+}
 
-// don't change below
 module.exports = Book;
